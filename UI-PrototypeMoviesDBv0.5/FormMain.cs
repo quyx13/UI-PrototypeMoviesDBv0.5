14a122f4b2d7849252d79b2937630265c69cec02
Thomas Ostendorf <quyx13@live.de>
2021-09-21 06:12:05 +0200

    z

--- UI-PrototypeMoviesDBv0.5/FormMain.cs
+++ UI-PrototypeMoviesDBv0.5/FormMain.cs
@@ -25,11 +25,31 @@ namespace UI_PrototypeMoviesDBv0._5
             controls.Add("", statusLabelInfo);
 
             worker = new Worker(controls);
+            worker.CreateControl();
+
+            //new Thread(DoWork).Start();
+            // TODO:String-Verarbeitung und DB-Work in getrennte Threads
+            // TODO:Bestätigungen per ShowDialog
+            // TODO:Konvertierung TSV zu XML und XML zu TSV (für movies.tsv)
+            // TODO:Custom-Dialog für Settings
+            // TODO:automatische Logs
+            // TODO:Selenium-Downloader einfügen
+            // TODO:Downloader prüft auf vollständige Datei
+            // TODO:HTMLs von Imdb auswählen, Downloader soll dann erst alle HTMLs des Films laden, dann weitergehen
         }
 
         private void FormMain_Load(object sender, EventArgs e)
         {
-            
+            statusProgressBar.Minimum = 0;
+            statusProgressBar.Maximum = 100;
+            statusProgressBar.Value = 0;
+
+            textBox.Clear();
+
+            statusLabelStopWatch.Text = "Ready";
+            statusLabelTask.Text = "";
+            statusLabelPercent.Text = "";
+            statusLabelInfo.Text = "";
         }
 
         private void FormMain_Shown(object sender, EventArgs e)
